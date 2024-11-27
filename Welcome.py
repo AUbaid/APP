c61201c4a4e4cfe3d1fcc7d1fa5af67645b1dfb7
AUbaid <60993511+AUbaid@users.noreply.github.com>
2024-11-27 17:43:04 +1100

    Add files via upload

---
+++ Welcome.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+
+st.title(' Mall Custmer Segmentation Project')
+
+def get_inputs():
+    
+    #get user inputs
+    lists = []
+    col_names = ['Gender' , 'Age', 'Annual Income (k$)','Spending Score (1-100)']
+    
+    Gender = st.text_input("Insert Gender")
+    Age = st.number_input('Insert Age')
+    Income = st.number_input('Insert Annual Income')
+    Score = st.number_input('Insert Spending Score [1-100]')
+    
+    lists.append(Gender)
+    lists.append(Age)
+    lists.append(Income)
+    lists.append(Score)
+    exo = pd.DataFrame(lists)
+    exo = exo.transpose()
+    exo.columns = col_names
+    st.write('The entered values are ', Gender, Age,Income,Score) 
+    st.write(exo) 
+#create predict button:
+    st.button("Reset", type="primary")
+    if st.button('Predict'):
+        with open("gmm_pipeline.pkl", "rb") as file:
+            reconstructed_model = pickle.load(file)    	  
+    	    pred= reconstructed_model.predict(exo)
+    	    st.subheader('Predicted Customer Cluster is: ', divider='rainbow')
+    	    st.subheader(pred)
+    
+    
+   
+    
+
+get_inputs()
+
+
